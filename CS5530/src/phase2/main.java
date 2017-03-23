395faebbf9f515c786be9780c224d4f670fec75e
executioNstyleZ <jared.kc.shaw@gmail.com>
2017-03-23 09:50:09 -0600

    menu flow for login/register done

--- CS5530/src/phase2/main.java
+++ CS5530/src/phase2/main.java
@@ -9,9 +9,15 @@ import cs5530.Course;
 
 public class main {
 
-	public static void displayMenu()
+	public static void displayLoginMenu(){
+		System.out.println("        Welcome to the Uotel System     ");
+		System.out.println("1. Login:");
+		System.out.println("2. Register:");
+   	 	System.out.println("please enter your choice:");
+	}
+	
+	public static void displayMainMenu()
 	{
-		 System.out.println("        Welcome to the UTrack System     ");
     	 System.out.println("1. search a course by cname and dname:");
     	 System.out.println("2. enter your own query:");
     	 System.out.println("3. exit:");
@@ -20,11 +26,16 @@ public class main {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Example for cs5530");
+
 		Connector con=null;
 		String choice;
         String cname;
         String dname;
+        String username;
+        String password;
+        String name;
+        String address;
+        String phoneNumber;
         String sql=null;
         int c=0;
          try
@@ -35,9 +46,72 @@ public class main {
 	         
 	             BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 	             
+	             //login
+	             while(c != 1 || c != 2){
+	            	 displayLoginMenu();
+	            	 while ((choice = in.readLine()) == null && choice.length() == 0);
+	            	 try{
+	            		 c = Integer.parseInt(choice);
+	            	 }catch (Exception e)
+	            	 {
+	            		 
+	            		 continue;
+	            	 }
+	            	 if (c<1 | c>2)
+	            		 continue;
+	            	 
+	            	 if(c == 1){//login
+	            		 System.out.println("Username:");
+	            		 while((username = in.readLine()) == null && username.length() == 0);
+	            		 System.out.println("Password:");
+	            		 while((password = in.readLine()) == null && password.length() == 0);
+	            		 
+	            		 Login log = new Login();
+	            		 if(log.login(username, password) == false){
+	            			 System.out.println("The username or password is incorrect");
+	            			 c = 0;
+	            		 }
+	            	 }
+	            	 else if(c == 2){//register
+	            		 System.out.println("Please choose a username:");
+	            		 while((username = in.readLine()) == null && username.length() == 0);
+	            		 
+	            		 Login log = new Login();
+	            		 //check for uniqueness of username
+	            		 while(log.checkForUsernameUniqueness(username) == false){
+
+	            			 System.out.println("The username " + username + " is already taken, please choose another username:");
+	            			 username = null;
+		            		 while((username = in.readLine()) == null && username.length() == 0);
+	            		 }
+	            		 System.out.println(username + " is unique");
+	            		 
+	            		 System.out.println("Please choose a password:");
+	            		 while((password = in.readLine()) == null && password.length() == 0);
+	            		 System.out.println("Please enter your full name:");
+	            		 while((name = in.readLine()) == null && name.length() == 0);
+	            		 System.out.println("Please enter your address:");
+	            		 while((address = in.readLine()) == null && address.length() == 0);
+	            		 System.out.println("Please enter your phone number:");
+	            		 while((phoneNumber = in.readLine()) == null && phoneNumber.length() == 0);
+	            		 
+	            		 if(log.registerNewUser(username, password, name, address, phoneNumber) == true){
+	            			 System.out.println(username + " was successfully added to the system");
+	            		 }else{
+	            			 System.out.println("There was an error adding " + username + " to the system, please try again");
+	            			 username = password = name = address = phoneNumber = null;
+	            			 c = 0;
+	            		 }
+	            	 }
+	             }
+	             
+	             //reset choice
+	             c = 0;
+
+	             //main menu
 	             while(true)
 	             {
-	            	 displayMenu();
+	            	 displayLoginMenu();
 	            	 while ((choice = in.readLine()) == null && choice.length() == 0);
 	            	 try{
 	            		 c = Integer.parseInt(choice);
