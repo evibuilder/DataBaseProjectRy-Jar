605c8bd4d19df81f31ce9caa573b85e862ae60d6
evibuilder <mrswimman563@gmail.com>
2017-03-26 18:39:13 -0600

    Feedback update
    Got the query commands for feed back ready.

--- CS5530/src/phase2/Feedback.java
+++ CS5530/src/phase2/Feedback.java
@@ -9,16 +9,21 @@ public class Feedback {
 	
 	// records the score and comments for the given TH from the given user
 	public void recordFeedback(String username, int idOfTH, String date, int score, String comments, Statement stmt){
-		
+		String sql="INSERT into Feedback(score, text, fbdate, hid, login) "
+				+ "VALUES ("+score+",'"+comments+"','"+date+"',"+idOfTH+",'"+username+"')";
 	}
 	
 	// rates the feedback from the given user, with a rating of 0,1, or 2
 	public void assessFeedback(String username, int feedbackID, int rating, Statement stmt){
-		
+		String sql="INSERT into Rates(login, fid, rating) "
+				+ "VALUES ('"+username+"',"+feedbackID+","+rating+")";
 	}
 	
 	//returns a list of the feedback for a given TH
 	public String showFeedbackForTH(int idOfTH, Statement stmt){
+		String sql = "SELECT * "
+				+ "FROM Feedback "
+				+ "WHERE hid = " + idOfTH;
 		return null;
 	}
 }
